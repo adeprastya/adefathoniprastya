218ef78afdfd0ab8426ca3b17994ac4ea542ec5d
Ade Fathoni Prastya <adefathoniprastya@gmail.com>
2024-06-12 02:15:17 +0700

    feat: add font, change text and style

--- src/pages/intro.jsx
+++ src/pages/intro.jsx
@@ -2,36 +2,36 @@ import Cursor from "../components/Cursor";
 import { useEffect, useState, useRef } from "react";
 import { motion } from "framer-motion";
 
-alert("Click to next");
+// alert("Click to next");
 
 const text = [
 	// HTML
-	"Hello World, I am on a journey to learn web development from scratch", // 0
+	"Hello World, My adventure into the world of web development begins now!", // 0
 
-	"<p>I remember how I started from basic concepts of <code>HTML</code></p>", // 1
+	"<p>Like a fresh coder cracking open a textbook, I devoured the basic building blocks of <code>HTML</code></p>", // 1
 
 	// CSS
-	"Then I began learn CSS and started to learn positioning and layouting", // 2
+	"Then, CSS entered the scene. I wielded it like a magic wand, conjuring layouts and positioning elements with precision", // 2
 
-	"Learning how to style elements for better user interface", // 3
+	"A canvas of possibilities, where colors paint emotions and typography shapes the message", // 3
 
-	"Transition, animation and many more...", // 4
+	"Like a scene-shifter in a play, transitions and animations bring it to life", // 4
 
-	"I also started to learn JavaScript, the most popular programming language", // 5
+	"JavaScript, the superhero, Buckle up and prepare to witness its power!", // 5
 
 	// JS
-	"Try to move cursor and you will see the power of JavaScript", // 6
+	"See that cursor? That's just a taste of JavaScript's magic in action", // 6
 
 	// Framer Motion
-	"Even you can drag this sentence around the screen, making it more interactive and engaging", // 7
+	"Even you can drag this sentence around the screen", // 7
 
-	"I was excited to learn and commit myself to this journey", // 8
+	"My hunger for knowledge is insatiable, and it fuels me to keep pushing forward", // 8
 
 	"However, my passion for mastering this skill has motivated me to keep learning", // 9
 
-	"Thank you for taking the time to read this", // 10
+	"Thank you for lending me your eyes on this odyssey", // 10
 
-	"Oops, I almost forgot, my name is", // 11
+	"Ooopsss, almost forgot to introduce myself! The name's...", // 11
 
 	"Ade Fathoni Prastya" // 12
 ];
@@ -39,11 +39,13 @@ const text = [
 const sty = {
 	base: "font-serif",
 	style1:
-		"font-serif p-10 sm:p-20 xl:p-40 bg-green-300 border-2 border-dashed border-black transition-all duration-1000 *:p-4 *:bg-blue-300 *:border-2 *:border-black *:transition-all *:duration-200",
+		"font-serif p-10 sm:p-20 xl:p-40 bg-green-300 border-2 border-dashed border-black transition-all duration-1000     *:p-4 *:bg-blue-300 *:border-2 *:border-black *:transition-all *:duration-200",
 	style2:
-		"box-border w-screen h-screen p-10 sm:p-20 xl:p-40 bg-slate-950 text-slate-50 font-sans font-thin text-center text-xl tracking-wider sm:text-2xl xl:text-4xl transition-all duration-500 *:mt-[calc(50vh-10rem)] *:translate-y-[50%] *:border-none *:bg-slate-950 *:transition-all *:duration-1000",
+		"box-border w-screen h-screen p-10 sm:p-20 xl:p-40 bg-slate-950 text-slate-50 font-cormorant text-center text-4xl tracking-wider sm:text-5xl xl:text-6xl transition-all duration-500     *:mt-[calc(50vh-1rem-8.5vw)] *:-translate-y-[50%] *:border-none *:bg-slate-950 *:transition-all *:duration-1000",
 	style3:
-		"box-border w-screen h-screen flex justify-center items-center p-10 sm:p-20 xl:p-40 bg-slate-950 text-slate-50 font-sans font-thin text-center text-xl tracking-wider sm:text-2xl xl:text-4xl transition-all duration-500 *:border-none *:transition-all *:duration-1000 *:animate-fade"
+		"box-border w-screen h-screen flex justify-center items-center p-10 sm:p-20 xl:p-40 bg-slate-950 text-slate-50 font-cormorant text-center text-4xl tracking-wider sm:text-5xl xl:text-6xl transition-all duration-500     *:inline *:border-none *:transition-all *:duration-1000 *:animate-fade     *:after:content-['...'] *:after:absolute *:after:inline *:after:animate-pulse",
+	style4:
+		"box-border w-screen h-screen flex justify-center items-center p-10 sm:p-20 xl:p-40 bg-slate-950 text-slate-50 font-cormorant text-center text-4xl tracking-wider sm:text-5xl xl:text-6xl transition-all duration-500     *:border-none *:transition-all *:duration-1000 *:animate-fade      *:first-letter:font-extrabold *:first-letter:text-5xl sm:*:first-letter:text-6xl xl:*:first-letter:text-7xl *:first-letter:text-transparent *:first-letter:bg-clip-text *:first-letter:bg-gradient-to-bl from-yellow-200 to-amber-500"
 };
 
 export default function Intro() {
@@ -65,13 +67,18 @@ export default function Intro() {
 
 	useEffect(() => {
 		if (index == 2) {
-			setStyle(sty.style1);
+			setTimeout(() => {
+				setStyle(sty.style1);
+			}, 500);
 		}
 		if (index == 3) {
 			setStyle(sty.style2);
 		}
-		if (index >= 4) {
+		if (index == 4) {
 			setStyle(sty.style3);
+		}
+		if (index >= 5) {
+			setStyle(sty.style4);
 		}
 	}, [index]);
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
 		extend: {
+			fontFamily: {
+				cormorant: ["Cormorant", "serif"],
+				epilogue: ["Epilogue", "sans"]
+			},
 			keyframes: {
 				fade: {
 					"0%": {
