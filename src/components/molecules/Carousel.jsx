53b14a02daeafbd403ba3b1bb614c5e169aa5f51
Ade Fathoni Prastya <adefathoniprastya@gmail.com>
2024-08-11 23:24:32 +0700

    refactor: Carousel code clarity and remove popmotion

--- src/components/molecules/Carousel.jsx
+++ src/components/molecules/Carousel.jsx
@@ -1,36 +1,93 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef, useCallback } from "react";
 import { motion, AnimatePresence } from "framer-motion";
-import { wrap } from "popmotion";
+import { wrap } from "@/utils/commonHelper";
 
-const swipeConfidenceThreshold = 7000;
-const swipePower = (offset, velocity) => {
-	return Math.abs(offset) * velocity;
+const sty = {
+	container: "relative overflow-hidden w-full h-full cursor-default",
+	img: "transform-gpu w-full h-full object-cover object-center",
+	navigation: "z-10 absolute top-1/2 left-[4%] -translate-y-1/2 flex flex-col gap-4",
+	navItem: "w-3 h-3 rounded-full backdrop-invert cursor-pointer"
 };
 
-export default function Carousel({ children, sources }) {
-	const [[page, direction], setPage] = useState([0, 0]);
-	const sourcesIndex = wrap(0, sources.length, page);
-	const containerRef = useRef(null);
-	const [height, setHeight] = useState(0);
-	const [isDelayed, setIsDelayed] = useState(false);
-	const intervalRef = useRef(null);
+const vars = {
+	enter: ([direction, height]) => ({
+		opacity: 0,
+		y: direction > 0 ? height : -height
+	}),
+	center: {
+		opacity: 1,
+		y: 0
+	},
+	exit: ([direction, height]) => ({
+		opacity: 0,
+		y: direction < 0 ? height : -height
+	}),
+	transition: {
+		ease: "easeInOut",
+		duration: 1.5
+	}
+};
 
-	const paginate = (newDirection) => {
-		setPage([page + newDirection, newDirection]);
+const SWIPE_CONFIDENCE_THRESHOLD = 10000;
+const swipePower = (offset, velocity) => Math.abs(offset) * velocity;
+
+export default function Carousel({ children, sources = [] }) {
+	const [[page, direction], setPage] = useState([0, 0]);
+	const pageWrapper = wrap(0, sources.length);
+	const containerRef = useRef();
+	const [height, setHeight] = useState(0);
+	const intervalRef = useRef();
+	const [isDragging, setIsDragging] = useState(false);
+
+	const paginate = useCallback(
+		(newDirection) => {
+			setPage((prev) => [pageWrapper(prev[0] + newDirection), newDirection]);
+		},
+		[pageWrapper]
+	);
+
+	const autoPaginate = useCallback(() => paginate(1), [paginate]);
+
+	const resetAutoPaginate = useCallback(() => {
+		clearInterval(intervalRef.current);
+		intervalRef.current = setInterval(autoPaginate, 5000);
+	}, [autoPaginate]);
+
+	const handleDragStart = () => {
+		clearInterval(intervalRef.current);
+		setIsDragging(true);
 	};
 
-	// Auto paginate
-	useEffect(() => {
-		const autoPaginate = () => {
-			if (!isDelayed) {
-				setPage((prev) => [prev[0] + 1, 1]);
+	const handleDragEnd = useCallback(
+		(offset, velocity) => {
+			const swipe = swipePower(offset.y, velocity.y);
+			if (Math.abs(swipe) > SWIPE_CONFIDENCE_THRESHOLD) {
+				const direction = swipe < 0 ? 1 : -1;
+				paginate(direction);
 			}
+
+			setIsDragging(false);
+			resetAutoPaginate();
+		},
+		[paginate, resetAutoPaginate]
+	);
+
+	const handleClick = useCallback(
+		(i) => {
+			setPage([i, 1]);
+			resetAutoPaginate();
+		},
+		[resetAutoPaginate]
+	);
+
+	// Run auto pagination
+	useEffect(() => {
+		resetAutoPaginate();
+
+		return () => {
+			clearInterval(intervalRef.current);
 		};
-
-		intervalRef.current = setInterval(autoPaginate, 5000);
-
-		return () => clearInterval(intervalRef.current);
-	}, [isDelayed]);
+	}, [resetAutoPaginate]);
 
 	// Get container height
 	useEffect(() => {
@@ -39,73 +96,31 @@ export default function Carousel({ children, sources }) {
 		}
 	}, [page]);
 
-	const handleClick = (i) => {
-		setPage([i, 1]);
-
-		clearInterval(intervalRef.current);
-
-		intervalRef.current = setInterval(() => {
-			setPage((prev) => [prev[0] + 1, 1]);
-		}, 5000);
-	};
-
-	const variants = {
-		enter: (direction) => ({
-			opacity: 0,
-			y: direction > 0 ? height : -height,
-			zIndex: 0
-		}),
-		center: {
-			opacity: 1,
-			y: 0,
-			zIndex: 1
-		},
-		exit: (direction) => ({
-			opacity: 0,
-			y: direction < 0 ? height / 2 : -height / 2,
-			zIndex: 0
-		})
-	};
-
 	return (
-		<div ref={containerRef} className="relative overflow-hidden w-full h-full cursor-default">
-			<AnimatePresence initial={false} custom={direction} mode="popLayout">
+		<div ref={containerRef} className={sty.container}>
+			<AnimatePresence initial={false} custom={[direction, height]} mode="popLayout">
 				<motion.img
-					key={page}
-					src={sources[sourcesIndex]}
-					custom={direction}
-					variants={variants}
+					key={`${page}-${direction}`}
+					src={sources[page]}
+					custom={[direction, height]}
+					variants={vars}
 					initial="enter"
 					animate="center"
 					exit="exit"
-					transition={{ ease: "easeInOut", duration: 1.5, zIndex: { duration: 0 } }}
+					transition={vars.transition}
 					drag="y"
 					dragConstraints={{ top: 0, bottom: 0 }}
 					dragElastic={0.5}
-					onDragStart={() => setIsDelayed(true)}
-					onDragEnd={(e, { offset, velocity }) => {
-						setIsDelayed(false);
-						const swipe = swipePower(offset.y, velocity.y);
-
-						if (swipe < -swipeConfidenceThreshold) {
-							paginate(1);
-						} else if (swipe > swipeConfidenceThreshold) {
-							paginate(-1);
-						}
-					}}
-					className={`transform-gpu relative w-full h-full object-cover object-center ${
-						isDelayed ? "cursor-grabbing" : "cursor-grab"
-					}`}
+					onDragStart={handleDragStart}
+					onDragEnd={(e, { offset, velocity }) => handleDragEnd(offset, velocity)}
+					className={`${sty.img} ${isDragging ? "cursor-grabbing" : "cursor-grab"}`}
 				/>
 			</AnimatePresence>
 
-			<div className="z-10 absolute top-[50%] left-[4%] translate-y-[-50%] flex flex-col gap-4">
+			{/* Navigation */}
+			<div className={sty.navigation}>
 				{sources.map((src, i) => (
-					<div
-						key={i}
-						onClick={() => handleClick(i)}
-						className={`w-3 h-3 rounded-full backdrop-invert cursor-pointer ${i === sourcesIndex ? "" : "opacity-25"}`}
-					/>
+					<div key={i} onClick={() => handleClick(i)} className={`${sty.navItem} ${i == page ? "" : "opacity-25"}`} />
 				))}
 			</div>
 
--- src/utils/commonHelper.js
+++ src/utils/commonHelper.js
@@ -1,3 +1,11 @@
 export function isMobile() {
 	return "ontouchstart" in window || navigator.maxTouchPoints > 0 || navigator.userAgent.includes("Mobi");
 }
+
+export function wrap(min, max) {
+	const range = max - min;
+
+	return (value) => {
+		return ((((value - min) % range) + range) % range) + min;
+	};
+}
