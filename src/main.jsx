11259fe807bb5f60709c2ab68f00ca195cbaf7b4
Ade Fathoni Prastya <adefathoniprastya@gmail.com>
2024-08-15 18:55:55 +0700

    removing react-router-dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "@/assets/css/index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ContextProvider from "@/contexts/ContextProvider";
 import App from "@/pages/app";
-
-const router = createBrowserRouter([
-	{
-		// path: "/adefathoniprastya",
-		path: "/",
-		element: <App />
-	}
-]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
 		<ContextProvider>
-			<RouterProvider router={router} />
+			<App />
 		</ContextProvider>
 	</React.StrictMode>
 );
