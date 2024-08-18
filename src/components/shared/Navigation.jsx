6d062a6ee389b5a701822de4152a05f890a99668
Ade Fathoni Prastya <adefathoniprastya@gmail.com>
2024-08-18 21:06:52 +0700

    fix(Navigation): remove flickering, restyle and reanimate

--- src/components/shared/Navigation.jsx
+++ src/components/shared/Navigation.jsx
@@ -29,23 +29,58 @@ const list = [
 ];
 
 const sty = {
-	container: "pointer-events-none z-50 fixed w-screen h-screen",
+	container: "pointer-events-none z-50 fixed w-full h-screen",
 
-	headerWrap: "pointer-events-auto absolute top-0 w-full p-2 pe-8 flex justify-between items-center",
-	logo: "z-50 w-10 sm:w-12 lg:w-14 aspect-square fill-slate-200 *:w-full *:h-full",
+	headerWrap: "pointer-events-auto absolute top-0 w-full p-4 flex justify-between items-center",
+	logo: "z-50 w-10 sm:w-12 lg:w-14 aspect-square fill-zinc-300 *:w-full *:h-full",
 
 	navWrap: "pointer-events-none absolute right-0",
-	listWrap:
-		"pointer-events-auto w-screen min-h-screen p-12 bg-slate-950/60 backdrop-blur-sm flex flex-col justify-between gap-8",
-	list: "relative font-cormorant font-bold tracking-widest leading-none text-slate-950 text-shadow-border text-shadow-slate-200     before:absolute before:top-1/2 before:-translate-y-1/2 before:-translate-x-24 before:w-4 before:aspect-square before:rounded-full before:border-2 before:border-slate-200     hover:text-shadow-yellow-400 hover:before:border-yellow-400",
+	listWrap: "pointer-events-auto w-screen min-h-screen bg-zinc-950/70 backdrop-blur-sm flex flex-col justify-between",
+	list: "ps-8 sm:ps-20 lg:ps-32 relative font-cormorant font-bold tracking-widest leading-none text-zinc-950 text-shadow-border text-shadow-zinc-200 before:absolute before:top-1/2 before:-translate-y-1/2 before:-translate-x-16 sm:before:-translate-x-32 lg:before:-translate-x-44 before:w-4 before:aspect-square before:rounded-full before:border-2 before:border-zinc-200 hover:bg-yellow-400 hover:text-shadow-yellow-400 hover:before:border-yellow-400",
 
 	chatWrap:
-		"pointer-events-auto z-20 absolute right-0 w-screen sm:max-w-[48rem] h-screen p-8 pe-12 pt-14 backdrop-blur-sm bg-slate-950/60 flex flex-col",
-	chatBox: "overflow-y-auto h-full p-4 rounded-t-xl border-2 border-b-0 border-slate-700 flex flex-col gap-4",
-	chatText: "w-fit p-2 rounded-lg bg-slate-300 font-fira tracking-wide text-base text-slate-950",
-	chatForm: "overflow-hidden h-16 rounded-b-xl border-2 border-slate-700 flex",
-	chatInput: "size-full px-4 bg-transparent font-fira text-lg text-slate-300 placeholder:text-slate-500",
-	chatButton: "h-full aspect-square fill-slate-950 bg-gradient-to-br from-yellow-300 to-amber-500 *:size-full"
+		"pointer-events-auto z-20 absolute right-0 w-full sm:max-w-[48rem] h-screen p-8 pe-12 pt-14 backdrop-blur-sm bg-zinc-950/70 flex flex-col",
+	chatBox: "overflow-y-auto h-full p-4 rounded-t-xl border-2 border-b-0 border-zinc-700 flex flex-col gap-4",
+	chatText: "w-fit p-2 rounded-lg bg-zinc-300 font-cormorant font-extrabold tracking-wide text-lg text-zinc-950",
+	chatForm: "overflow-hidden h-16 rounded-b-xl border-2 border-zinc-700 flex",
+	chatInput:
+		"size-full px-4 bg-transparent font-cormorant font-extrabold text-lg text-zinc-300 placeholder:text-zinc-500",
+	chatButton: "h-full aspect-square fill-zinc-950 bg-gradient-to-br from-yellow-300 to-amber-500 *:size-full"
+};
+
+const vars = {
+	ul: {
+		hidden: {
+			x: "100%",
+			paddingTop: "30vh",
+			paddingBottom: "30vh"
+		},
+		visible: {
+			x: 0,
+			paddingTop: "10vh",
+			paddingBottom: "10vh"
+		}
+	},
+	li: {
+		hidden: {
+			fontSize: "1rem"
+		},
+		visible: {
+			fontSize: "5rem"
+		}
+	},
+	chat: {
+		hidden: {
+			x: "100%"
+		},
+		visible: {
+			x: 0
+		}
+	},
+	transition: {
+		duration: 0.3,
+		ease: "easeInOut"
+	}
 };
 
 export default function Navigation() {
@@ -60,6 +95,7 @@ export default function Navigation() {
 		setIsChatOpen((n) => !n);
 	};
 
+	// Scroll Effect & Active Navigation
 	useEffect(() => {
 		const handleScroll = () => {
 			document.querySelectorAll("section").forEach((section, index) => {
@@ -77,8 +113,11 @@ export default function Navigation() {
 
 	return (
 		<header className={sty.container}>
+			{/* Header */}
 			<div className={sty.headerWrap}>
 				<button
+					type="button"
+					aria-label="Home"
 					onClick={(e) => {
 						e.preventDefault();
 						window.location.reload();
@@ -90,13 +129,18 @@ export default function Navigation() {
 
 				<div className="flex gap-4">
 					<motion.button
+						type="button"
+						aria-label="AI Chat"
 						onClick={handleChat}
 						className={sty.logo}
 						animate={isChatOpen ? { rotate: 135 } : { rotate: 0 }}
 					>
 						<StarIcon />
 					</motion.button>
+
 					<motion.button
+						type="button"
+						aria-label="Menu"
 						onClick={handleMenu}
 						className={sty.logo}
 						animate={isNavOpen ? { rotate: 180 } : { rotate: 0 }}
@@ -106,10 +150,14 @@ export default function Navigation() {
 				</div>
 			</div>
 
+			{/* Navigation Menu */}
 			<nav className={sty.navWrap}>
 				<motion.ul
 					className={sty.listWrap}
-					animate={isNavOpen ? { x: 0 } : { x: "100%", paddingTop: "30vh", paddingBottom: "30vh" }}
+					variants={vars.ul}
+					initial="hidden"
+					animate={isNavOpen ? "visible" : "hidden"}
+					transition={vars.transition}
 				>
 					{list.map((list, i) => (
 						<a
@@ -121,8 +169,11 @@ export default function Navigation() {
 							href={list.link}
 						>
 							<motion.li
-								className={`${sty.list} ${activeNav === i ? " before:border-yellow-400 text-shadow-yellow-400" : ""}`}
-								animate={isNavOpen ? { fontSize: "5rem" } : { fontSize: "1rem" }}
+								className={`${sty.list} ${activeNav == i ? " before:border-amber-400 text-shadow-yellow-400" : ""}`}
+								variants={vars.li}
+								initial="hidden"
+								animate={isNavOpen ? "visible" : "hidden"}
+								transition={vars.transition}
 							>
 								{list.title}
 							</motion.li>
@@ -131,6 +182,7 @@ export default function Navigation() {
 				</motion.ul>
 			</nav>
 
+			{/* AI Chat Box */}
 			<ChatBox open={isChatOpen} />
 		</header>
 	);
@@ -151,7 +203,14 @@ function ChatBox({ open }) {
 	};
 
 	return (
-		<motion.div className={sty.chatWrap} animate={open ? { x: 0 } : { x: "100%" }} onSubmit={(e) => handleSubmit(e)}>
+		<motion.div
+			className={sty.chatWrap}
+			variants={vars.chat}
+			initial="hidden"
+			animate={open ? "visible" : "hidden"}
+			transition={vars.transition}
+			onSubmit={(e) => handleSubmit(e)}
+		>
 			<div className={sty.chatBox}>
 				{chat.map((c, i) => (
 					<p
