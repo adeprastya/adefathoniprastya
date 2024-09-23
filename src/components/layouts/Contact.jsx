5105fb5a9232622a236f89ac0516625cec727990
Ade Fathoni Prastya <adefathoniprastya@gmail.com>
2024-09-23 20:16:52 +0700

    restyle Contact

--- src/components/layouts/Contact.jsx
+++ src/components/layouts/Contact.jsx
@@ -1,4 +1,4 @@
-import { motion, useScroll, useTransform } from "framer-motion";
+import { motion, useMotionTemplate, useScroll, useTransform } from "framer-motion";
 import { useRef } from "react";
 
 const data = [
@@ -7,70 +7,72 @@ const data = [
 		link: "mailto:adefathoniprastya@gmail.com"
 	},
 	{
+		content: "Telegram",
+		link: "https://t.me/adefathoniprastya"
+	},
+	{
+		content: "Discord",
+		link: "https://discordapp.com/users/adeprastya"
+	},
+	{
 		content: "Linkedin",
 		link: "https://www.linkedin.com/in/adefathoniprastya"
 	},
 	{
-		content: "Telegram",
-		link: "https://t.me/adefathoniprastya"
+		content: "Github",
+		link: "https://github.com/adeprastya"
 	}
 ];
 
 const sty = {
-	container: "w-full h-[200vh]",
+	container: "w-full min-h-screen flex flex-col lg:flex-row",
 
-	contentWrapper: "sticky top-0 flex flex-col lg:flex-row",
-
-	textWrapper: "pt-20 lg:pt-0 w-full h-[50vh] lg:h-screen flex justify-center items-center",
+	textWrapper: "sticky top-0 w-full h-[50vh] lg:h-screen py-10 lg:px-10 flex justify-center items-center",
 	heading:
-		"font-cormorant font-bold tracking-wide text-[4rem] sm:text-[5rem] md:text-[6rem] xl:text-[6rem] text-transparent bg-clip-text bg-gradient-to-bl from-amber-500 to-yellow-300     flex flex-col w-fit",
+		"font-serif tracking-wide text-[4rem] sm:text-[5rem] md:text-[6rem] xl:text-[6rem] text-center lg:text-start text-zinc-200 flex flex-col w-fit",
 
-	itemWrapper: "pb-20 lg:pb-0 w-full h-[50vh] lg:h-screen flex flex-col justify-center items-center",
-	item: "absolute font-cormorant font-bold tracking-wide text-[5rem] sm:text-[6rem] md:text-[7rem] xl:text-[8rem] text-zinc-300 transform-gpu"
+	itemWrapper:
+		"w-full min-h-screen py-[40vh] lg:py-[70vh] lg:px-10 flex flex-col justify-center items-center gap-[5vh]",
+	item: "transform-gpu font-sans font-bold tracking-wide text-[5rem] sm:text-[6rem] md:text-[7rem] xl:text-[8rem] text-transparent bg-clip-text bg-gradient-to-br from-zinc-200 via-yellow-400 to-amber-500"
 };
 
-export default function Contact() {
+function Item({ data }) {
 	const ref = useRef(null);
-	const { scrollYProgress } = useScroll({ target: ref, offset: ["start start", "end end"] });
-	const y = [
-		useTransform(scrollYProgress, [0, 0.5, 1], ["0%", "-100%", "-200%"]),
-		useTransform(scrollYProgress, [0, 0.5, 1], ["100%", "0%", "-100%"]),
-		useTransform(scrollYProgress, [0, 0.5, 1], ["-200%", "100%", "0%"])
-	];
-	const opacity = [
-		useTransform(scrollYProgress, [0, 0.5, 1], [1, 0, 0]),
-		useTransform(scrollYProgress, [0, 0.5, 1], [0, 1, 0]),
-		useTransform(scrollYProgress, [0, 0.5, 1], [0, 0, 1])
-	];
-	const rotate = [
-		useTransform(scrollYProgress, [0, 0.5, 1], ["0deg", "60deg", "120deg"]),
-		useTransform(scrollYProgress, [0, 0.5, 1], ["-60deg", "0deg", "60deg"]),
-		useTransform(scrollYProgress, [0, 0.5, 1], ["-120deg", "60deg", "0deg"])
-	];
+	const { scrollYProgress } = useScroll({
+		target: ref,
+		offset: window.innerWidth > 1024 ? ["0 0", "1 1"] : ["0 0.9", "1 0.5"]
+	});
+
+	const y = useTransform(
+		scrollYProgress,
+		[0, 0.25, 0.5, 0.75, 1],
+		window.innerWidth > 1024 ? [10, 0.5, 0, -0.5, -10] : [-60, -20, 0, 20, 60]
+	);
+	const opacity = useTransform(scrollYProgress, [0, 0.5, 1], [0, 1, 0]);
+	const rotate = useTransform(scrollYProgress, [0, 0.5, 1], [60, 0, 60]);
+	const transform = useMotionTemplate`translateY(${y}%) rotateX(${rotate}deg)`;
 
 	return (
-		<section ref={ref} id="contact" className={sty.container}>
-			<div className={sty.contentWrapper}>
-				<div className={sty.textWrapper}>
-					<h1 className={sty.heading}>
-						<span>Get In Touch</span>
-						<span>With Me</span>
-					</h1>
-				</div>
+		<motion.a ref={ref} href={data.link} target="_blank" style={{ opacity, transform }} className={sty.item}>
+			{data.content}
+		</motion.a>
+	);
+}
 
-				<div className={sty.itemWrapper}>
-					{data.map((data, i) => (
-						<motion.a
-							key={i}
-							href={data.link}
-							target="_blank"
-							style={{ y: y[i], rotateX: rotate[i], opacity: opacity[i] }}
-							className={sty.item}
-						>
-							{data.content}
-						</motion.a>
-					))}
-				</div>
+export default function Contact() {
+	return (
+		<section id="contact" className={sty.container}>
+			<div className={sty.textWrapper}>
+				<h1 className={sty.heading}>
+					<span>Get In Touch</span>
+					<span>With Me</span>
+				</h1>
+			</div>
+
+			<div className={sty.itemWrapper}>
+				{data.map((data, i) => (
+					<Item key={i} data={{ ...data, i }} />
+				))}
 			</div>
 		</section>
 	);
