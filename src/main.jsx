c754ebb8ada98249167516f0222cfa0335a5c830
Ade Fathoni Prastya <adefathoniprastya@gmail.com>
2024-10-17 18:23:14 +0700

    add Vercel Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import ReactDOM from "react-dom/client";
 import "@/assets/css/index.css";
 import ContextProvider from "@/contexts/ContextProvider";
 import App from "@/App";
+import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
+		<Analytics />
+
 		<ContextProvider>
 			<App />
 		</ContextProvider>
